44f5bb3a9ccdeedba0ef7814558803c4080bd70f
Marco Sartiano <msartianouk@gmail.com>
2020-02-15 03:44:34 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+                '@babel/preset-env',
+                '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
